9b8ec4dabd69652858128ad342dad052635621cc
Alex <ab1425@canterbury.ac.uk>
2023-12-07 01:04:02 +0000

    Add dropdown styling

--- WebApp/app/src/lib/components/PeopleDropdown.svelte
+++ WebApp/app/src/lib/components/PeopleDropdown.svelte
@@ -1,28 +1,33 @@
 <script>
-	import { createEventDispatcher } from "svelte";
-	import { onMount } from "svelte";
+  import { createEventDispatcher } from "svelte";
+  import { onMount } from "svelte";
 
-	export let people;
+  export let people;
 
-	let personId;
+  let personId;
 
-	const dispatch = createEventDispatcher();
+  const dispatch = createEventDispatcher();
 
-	function SendPerson() {
-		dispatch("personSelected", { personId: personId });
-	}
+  function SendPerson() {
+    dispatch("personSelected", { personId: personId });
+  }
 </script>
 
-<select bind:value={personId}>
-	{#each people as person}
-		<option value={person.id}
-			>{person.name[0].toUpperCase() + person.name.substr(1)}</option
-		>
-	{/each}
-</select>
+<div class="flex flex-col items-center mt-10">
+  <select
+    bind:value={personId}
+    class="bg-slate-800 text-xl w-52 p-2 rounded-md"
+  >
+    {#each people as person}
+      <option value={person.id}
+        >{person.name[0].toUpperCase() + person.name.substr(1)}</option
+      >
+    {/each}
+  </select>
 
-<button
-	class="w-max text-lg rounded-md bg-emerald-800 hover:bg-emerald-600 py-2 px-4 transition-colors ease-in duration-75 disabled:bg-gray-600"
-	on:click={SendPerson}
-	disabled={!personId}>Continue</button
->
+  <button
+    class="mt-5 w-max text-lg rounded-md bg-emerald-800 hover:bg-emerald-600 py-2 px-4 transition-colors ease-in duration-75 disabled:bg-gray-600"
+    on:click={SendPerson}
+    disabled={!personId}>Continue</button
+  >
+</div>
