0e9e260f10150a8f750ff01ec925b342a594f81e
Alex <78022094+Alex-Bartlett@users.noreply.github.com>
2023-12-19 17:45:34 +0000

    Add further checks to prevent duplicate votes

--- WebApp/app/src/routes/(auth)/vote/[categoryId]/+page.server.js
+++ WebApp/app/src/routes/(auth)/vote/[categoryId]/+page.server.js
@@ -1,4 +1,18 @@
+import { redirect } from '@sveltejs/kit'
+
 export const load = async ({ params, fetch, locals }) => {
+	// Redirect to votes page if user has a vote for this category
+	const userAlreadyVoted = await hasUserAlreadyVoted();
+	if (userAlreadyVoted) {
+		throw redirect(303, '/vote')
+	}
+
+	async function hasUserAlreadyVoted() {
+		const res = await fetch(`/api/categories?unvotedBy=${locals.user.person_id}`)
+		const data = await res.json();
+		const ids = data.map((x) => x.id);
+		return !ids.includes(Number(params.categoryId))
+	}
 	const fetchCategory = async () => {
 		const res = await fetch(`/api/categories/${params.categoryId}`);
 		const data = await res.json();
