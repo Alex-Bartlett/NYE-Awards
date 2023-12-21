c2aa48a36984c3cbe0fd9e9059fc1a37b51cabd6
Alex <78022094+Alex-Bartlett@users.noreply.github.com>
2023-12-21 15:46:45 +0000

    Fix peopleDropdown not setting default value properly

--- WebApp/app/src/lib/components/PeopleDropdown.svelte
+++ WebApp/app/src/lib/components/PeopleDropdown.svelte
@@ -1,27 +1,29 @@
 <script>
-  import { createEventDispatcher } from "svelte";
+	import { createEventDispatcher } from "svelte";
 
-  export let people;
+	export let people;
 
-  let personId;
+	let personId = people[0].id;
 
-  const dispatch = createEventDispatcher();
+	const dispatch = createEventDispatcher();
 
-  function SendPerson() {
-    dispatch("personSelected", { personId: personId });
-  }
+	function SendPerson() {
+		dispatch("personSelected", { personId: personId });
+	}
+
+	SendPerson();
 </script>
 
 <div class="flex flex-col items-center">
-  <select
-    bind:value={personId}
-    on:change={SendPerson}
-    class="bg-slate-800 text-xl w-52 p-2 rounded-md"
-  >
-    {#each people as person}
-      <option value={person.id}
-        >{person.name[0].toUpperCase() + person.name.substr(1)}</option
-      >
-    {/each}
-  </select>
+	<select
+		bind:value={personId}
+		on:change={SendPerson}
+		class="bg-slate-800 text-xl w-52 p-2 rounded-md"
+	>
+		{#each people as person}
+			<option value={person.id}
+				>{person.name[0].toUpperCase() + person.name.substr(1)}</option
+			>
+		{/each}
+	</select>
 </div>
