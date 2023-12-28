363eee45475b4ec6d3d94060894d3255c1a48d9c
Alex <ab1425@canterbury.ac.uk>
2023-12-28 01:18:33 +0000

    Add next-round page and api route

--- WebApp/app/src/routes/(auth)/(mod)/(admin)/rounds/+page.svelte
+++ WebApp/app/src/routes/(auth)/(mod)/(admin)/rounds/+page.svelte
@@ -1,14 +1,27 @@
 <script>
   import { onMount } from "svelte";
+  import { goto } from "$app/navigation";
 
   let confirm = false;
   let round = "checking...";
+  let submitting = false;
 
   function Confirm() {
     confirm = true;
   }
 
-  async function NextRound() {}
+  async function NextRound() {
+    submitting = true;
+    // Increment the round and duplicate winning quotes for the next round (see api route for full workings)
+    await fetch("/api/game/next-round", {
+      method: "PUT",
+      body: JSON.stringify({
+        confirm: true,
+      }),
+    });
+
+    goto(".");
+  }
 
   onMount(async () => {
     const res = await fetch("/api/game");
@@ -23,8 +36,10 @@
     {#if !confirm}
       Once the current round of voting is complete, click below to increment the
       round, unlocking the next round of voting.
-    {:else}
+    {:else if !submitting}
       Are you sure?
+    {:else}
+      Updating the database. This may take a moment, please wait...
     {/if}
   </p>
   <div class="flex justify-center w-1/2 xl:w-1/6 text-center text-lg">
@@ -34,7 +49,7 @@
         class="rounded-md bg-emerald-800 hover:bg-emerald-600 py-2 px-4 w-auto inline-block transition-colors ease-in duration-75"
         >Proceed to next round</button
       >
-    {:else}
+    {:else if !submitting}
       <button
         on:click={NextRound}
         class="mr-2 rounded-md bg-emerald-800 hover:bg-emerald-600 py-2 px-4 w-full inline-block transition-colors ease-in duration-75"
